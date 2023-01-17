e29b21cc5b8e45c3aa962be0e454c3893660dd70
Divya <divya5guntaka@gmail.com>
2023-01-17 10:35:40 +0530

    Update

--- data.tf
+++ data.tf
@@ -3,3 +3,8 @@ data "aws_ami" "centos8" {
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
+
+data "aws_route53_zone" "main" {
+  name         = var.domain
+  private_zone = false
+}
