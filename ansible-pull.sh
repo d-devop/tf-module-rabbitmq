cf26626084fbb0ac4856971a54d79dd78d7c7f41
d-devop <divya5guntaka@gmail.com>
2023-01-27 15:16:51 +0530

    Update

--- ansible-pull.sh
+++ ansible-pull.sh
@@ -2,4 +2,4 @@
 touch /opt/user-data.log
 id >>/opt/user-data.log
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/d-devop/roboshop-ansible roboshop.yml -e ROLE_NAME=${component} -e ENV=${env} | tee -a /opt/user-data.log
+ansible-pull -i localhost, -U https://github.com/d-devop/roboshop-ansible roboshop.yml -e ROLE_NAME=rabbitmq -e ENV=${env} | tee -a /opt/user-data.log
